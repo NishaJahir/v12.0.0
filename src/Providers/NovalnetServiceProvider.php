766c8227e3f6b730f8d8c251e51b6e515565fc09
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2021-04-26 11:25:54 +0530

    Update NovalnetServiceProvider.php

--- src/Providers/NovalnetServiceProvider.php
+++ src/Providers/NovalnetServiceProvider.php
@@ -260,7 +260,7 @@ class NovalnetServiceProvider extends ServiceProvider
                                         'nnPaymentProcessUrl' => $paymentService->getProcessPaymentUrl(),
                                         'paymentMopKey'       =>  $paymentKey,
                                         'paymentName'         => $paymentName,
-                                        'guaranteePayment'    => $paymentService->checkGuaranteePaymentDisplayStatus($basket, strtolower($paymentKey));
+                                        'guaranteePayment'    => $paymentService->checkGuaranteePaymentDisplayStatus($basket, strtolower($paymentKey)),
                                         'ccFormDetails'       => !empty($ccFormDetails) ? $ccFormDetails : '',
                                         'ccCustomFields'       => !empty($ccCustomFields) ? $ccCustomFields : '',
                                         'customerNo'           => $customerAccount->getAccountContactId(),
